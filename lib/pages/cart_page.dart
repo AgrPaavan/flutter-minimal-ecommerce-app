bd1571f1dab84b8df50fcf74eaaf578859db39f9
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2024-09-10 16:02:37 +0530

    Refactor CartPage to display a list of products in the cart and add a pay button

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_ecommerce_app/components/my_buttons.dart';
+import 'package:minimal_ecommerce_app/models/shop.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  void payButtonPressed(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('Payment Successful'),
+              content: const Text('Thank you for shopping with us!'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    context.read<Shop>().clearCart();
+                    Navigator.pop(context);
+                  },
+                  child:
+                      const Text('OK', style: TextStyle(color: Colors.black)),
+                ),
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
+    final cart = context.watch<Shop>().cart;
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
@@ -12,6 +37,38 @@ class CartPage extends StatelessWidget {
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         elevation: 0,
         title: const Text("My Cart"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: cart.isEmpty
+                ? const Center(
+                    child: Text("No items in you cart"),
+                  )
+                : ListView.builder(
+                    itemCount: cart.length,
+                    itemBuilder: (context, index) {
+                      final product = cart[index];
+                      return ListTile(
+                        title: Text(product.name),
+                        subtitle: Text(product.price.toString()),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.remove),
+                          onPressed: () {
+                            context.read<Shop>().removeFromCart(product);
+                          },
+                        ),
+                      );
+                    },
+                  ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 25.0),
+            child: MyButton(
+                onTap: () => payButtonPressed(context),
+                child: const Text("PAY NOW")),
+          ),
+        ],
       ),
     );
   }
