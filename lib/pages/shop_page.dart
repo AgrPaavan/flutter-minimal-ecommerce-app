ae142ab2cdf39d3d113e5e5a772a3ca720d329d3
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2024-09-10 11:58:03 +0530

    Rename home_page.dart to shop_page.dart

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ShopPage extends StatelessWidget {
+  const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
