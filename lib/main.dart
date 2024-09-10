c6285ec40565368ecaf5be05d3788fded5889c8f
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2024-09-10 12:15:53 +0530

    Add CartPage component

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_ecommerce_app/pages/cart_page.dart';
 import 'package:minimal_ecommerce_app/pages/intro_page.dart';
 import 'package:minimal_ecommerce_app/pages/shop_page.dart';
 import 'package:minimal_ecommerce_app/themes/light_mode.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/intro_page': (context) => const IntroPage(),
         '/shop_page': (context) => const ShopPage(),
+        '/cart_page': (context) => const CartPage(),
       },
     );
   }
